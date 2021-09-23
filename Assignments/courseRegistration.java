09111a6ecd6c61c36805f772338f1c16fa94791a
Sanika Patil <sanikapatil1021@gmail.com>
2021-09-23 21:34:08 +0530

    Update courseRegistration.java

--- Assignments/courseRegistration.java
+++ Assignments/courseRegistration.java
@@ -1,1 +1,15 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+    public class enroll{
+     string name;
+     string cname;
+     enroll()
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
 
