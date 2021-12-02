7df8f9f7350ce01ad236d99c2226cd4ce7d5af32
Sanika Patil <sanikapatil1021@gmail.com>
2021-12-02 15:36:26 +0530

    Create genericTemplates.java

---
+++ Assignments/genericTemplates.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+ class operationInt<T>{
+	T obj;
+	
+}
+    
+public class generic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter \n 1 : String \n 2 : Integer \n Enter option :\n");
+		int opt = sc.nextInt();
+		ArrayList<Integer> list1 = new ArrayList();
+        
+	}
+
+}
